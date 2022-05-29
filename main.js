24a3cdeebe427443909caffc608eac3ef1d701d4
NetanelYaso <netanelyaso12@gmail.com>
2022-05-29 22:36:36 +0300

    DOM

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log(h1_id.innerText);
+
+document.write(h3_id.innerText +'my dom app');
+
+var team = document.getElementsByClassName('my_team');
+console.log(team);
+
+console.log(team[1])
+// תחזור לשאלה 5
+
+
+
+
+
+
+
